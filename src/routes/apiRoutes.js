92299f1648c82041556936f6457bd19c60e3b120
HyagoGabriel <hyagogabriel3@gmail.com>
2025-04-23 09:06:03 -0300

    atualizaçao_23_04

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -18,7 +18,7 @@ router.delete("/org/:id_organizador", orgController.deleteOrg);
 
 router.post("/evento", eventoController.createEvento);
 router.get("/evento", eventoController.getAllEventos);
-router.get("/evento/data", eventoController.getEventosPorData);
+router.get("/evento/data", eventoController.getEventosPorData); 
 router.get("/evento/:data", eventoController.getEventosPorData7Dias);
 router.put("/evento", eventoController.updateEvento);
 router.delete("/evento/:id_evento", eventoController.deleteEvento);
