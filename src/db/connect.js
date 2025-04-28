392f105aa89f4297e41e23371678e1443dba41b9
HyagoGabriel <hyagogabriel3@gmail.com>
2025-04-28 12:40:22 -0300

    28_04

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    connectionLimit:10,
-    host:'localhost',
-    user:'alunods',
-    password:'ma@12345',
-    database:'vio_hyago'
-})
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 module.exports = pool;
